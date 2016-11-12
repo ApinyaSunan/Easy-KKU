7c08a40c9dea912792052e0c298e428f31cdc092
Apinya <sunanyuenyong.apinya@gmail.com>
2016-11-12 13:43:16 +0700

    Sign Up UI finish

--- app/src/main/java/kku/sunanyuenyong/apinya/easykku/SignUpActivity.java
+++ app/src/main/java/kku/sunanyuenyong/apinya/easykku/SignUpActivity.java
@@ -2,12 +2,17 @@ package kku.sunanyuenyong.apinya.easykku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditTest, phoneEditText, userEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+    }// Main Method
+}// Main Class
